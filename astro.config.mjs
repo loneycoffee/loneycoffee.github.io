30aa320787f89dba0f057d840c10159bec8b0a7a
loneycoffee <towdev@proton.me>
2023-05-15 19:19:52 +0200

    Edit domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://tomt.fr',
+  site:'https://loneycoffee.github.io',
 });
 
